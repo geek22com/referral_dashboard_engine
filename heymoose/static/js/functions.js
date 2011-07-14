88b7c0ddf3b2da511b266bcef0e9c87d79ecf3e2
kshilov <ks.shilov@gmail.com>
2011-07-14 15:48:03 +0400

    version 0.1

---
+++ heymoose/static/js/functions.js
@@ -0,0 +1,12 @@
+function showTooltip()
+{
+	var myDiv = document.getElementById('tooltip');
+	if(myDiv.style.display == 'none')
+	{
+		myDiv.style.display = 'block';
+	} else {
+		myDiv.style.display = 'none';
+	}
+		return false;
+}
+

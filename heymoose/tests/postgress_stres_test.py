03ce06e3bf55d71e8396c9e90ff6bd9ffd405cc0
kshilov <ks.shilov@gmail.com>
2011-07-18 15:14:00 +0400

    Add time interval measuring and debugging for postgress tests

--- heymoose/tests/postgress_stres_test.py
+++ heymoose/tests/postgress_stres_test.py
@@ -11,12 +11,17 @@ from heymoose.db.models import FeedBack
 import random
 import string
 import sys
+from datetime import datetime
 
 def gen_random_mail(ln=10):
 	return ''.join(random.choice(string.letters) for i in xrange(ln)) + "@" + ''.join(random.choice(string.letters) for i in xrange(ln)) + ".ru"
 
 def gen_random_username(ln=20):
 	return ''.join(random.choice(string.letters) for i in xrange(ln))
+
+def str_diff_datetime(before, after):
+	diff = after - before
+	return " at seconds:" + str(diff.seconds) + "  microseconds:" + str(diff.microseconds)
 
 @frontend.route('/test_register', methods=['GET', 'POST'])
 def test_register():
@@ -27,11 +32,14 @@ def test_register():
 			user = User(username=username,
 						email=email,
 						passwordhash=generate_password_hash("test_register"))
+			before = datetime.now()
 			user.save()
+			after = datetime.now()
 		except:
 			return str(sys.exc_info())
-
-		return "test_register Success"
+		s_mess = "test_register Success " + str_diff_datetime(before=before, after=after)
+		app_logger.debug(s_mess)
+		return s_mess
 
 @frontend.route('/test_show_category/<category_id>/<pagenum>')
 def test_show_category(category_id=None, pagenum=None):
@@ -42,27 +50,45 @@ def test_show_category(category_id=None, pagenum=None):
 	if pagenum and int(pagenum) > 0:
 		offset = int(pagenum) * 10
 
+	before = datetime.now()
 	blogs = Blog.load_blogs_by_category(category_id=category_id, offset=offset)
+	after = datetime.now()
+	load_blogs_str = " load_blogs_by_category " + str_diff_datetime(before=before, after=after)
+
 	if blogs:
 		g.params['blogs'] = blogs
 
+	before = datetime.now()
 	categories = Category.load_categories()
+	after = datetime.now()
+	load_categories = " load_categories " + str_diff_datetime(before=before, after=after)
 	if categories:
 		g.params['categories'] = categories
 
+	before = datetime.now()
 	category = Category.load_category(category_id)
+	after = datetime.now()
+	load_category = " load_category " + str_diff_datetime(before=before, after=after)
 	if category:
 		g.params['category'] = category
 
-	return "test_show_category Success"
+	s_mess = "test_show_category Success " + \
+		load_blogs_str + ":::" + \
+		load_categories + ":::" + \
+		load_category + "xxx"
+	app_logger.debug(s_mess)
+	return s_mess
 
 @frontend.route('/test_login/<username>', methods=['GET', 'POST'])
 def test_login(username):
 	if username is None:
 		return "test_login Fail"
-
+	before = datetime.now()
 	user = User.get_user(username)
-	return "test_login Success"
+	after = datetime.now()
+	s_mess = "test_login Success " + str_diff_datetime(before=before, after=after)
+	app_logger.debug(s_mess)
+	return s_mess
 
 @frontend.route('/test_feedback', methods=['GET', 'POST'])
 def test_feedback():
@@ -72,10 +98,13 @@ def test_feedback():
 		comment = gen_random_username(256)
 		feedback = FeedBack(email = email,
 							comment = comment)
+		before = datetime.now()
 		feedback.save_new()
-		return "test_feedback Success"
+		after = datetime.now()
 	except:
 		return str(sys.exc_info())
 
-
+	s_mess = "test_feedback Success " + str_diff_datetime(before=before, after=after)
+	app_logger.debug(s_mess)
+	return s_mess
 

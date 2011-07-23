450a78c25a370f644bbcf440cec0b77535967985
kshilov <ks.shilov@gmail.com>
2011-07-24 02:35:22 +0400

    New design deployd and tested on server

--- heymoose/settings/debug_config.py
+++ heymoose/settings/debug_config.py
@@ -6,8 +6,8 @@ DATABASE = 'social_sampler'
 DEBUG = True
 DB_USER = 'qa'
 DB_PASSWORD = 'appatit'
-DB_PORT = 5432
+DB_PORT = 6432
 DB_CHARSET = 'utf8'
-SECRET_KEY = 'secret key'
+SECRET_KEY = 'lola pola_mola_cola'
 print "ATTENTION DEBUG CONFIG IS USED"
 

7657467ba97d0f92ef4f3a0fba7d16cc5a5ffdbf
kshilov <ks.shilov@gmail.com>
2011-09-06 18:24:07 +0400

    fine point

--- heymoose/db/xml_parser.py
+++ heymoose/db/xml_parser.py
@@ -1,5 +1,7 @@
 from lxml import etree
 
+#TODO: write correct xml mapper
+
 def xpath_to_string(xml_string, path): #TODO: read lxml doc...
 	document = etree.fromstring(xml_string)
 	element = document.xpath(path)

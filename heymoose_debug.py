f4f4d1f8056d5361f552d99ba5d0d0a2d183183b
iceberg <iceberg@boss.(none)>
2011-10-19 00:38:42 +0400

    v0.1

--- heymoose_debug.py
+++ heymoose_debug.py
@@ -1,7 +1,7 @@
 import sys, os
-cmd_folder = os.path.dirname(os.path.abspath("/home/ksshilov/heymoose_debug/frontend/frontend-1.0/heymoose"))
+cmd_folder = os.path.dirname(os.path.abspath("/home/iceberg/heymoose/heymoose"))
 if cmd_folder not in sys.path:
 	sys.path.insert(0, cmd_folder)
 import __builtin__
-__builtin__.DEBUG_CONFIG="/home/ksshilov/heymoose_debug/frontend/frontend-1.0/config_debug.py"
+__builtin__.DEBUG_CONFIG="/home/iceberg/heymoose/config_debug.py"
 from heymoose import app as application
--- runserver.py
+++ runserver.py
@@ -1,14 +1,14 @@
 import sys, os
-cmd_folder = os.path.dirname(os.path.abspath("/home/kshilov/PycharmProjects/frontend_new_templates/heymoose"))
+cmd_folder = os.path.dirname(os.path.abspath("/home/iceberg/heymoose/heymoose"))
 if cmd_folder not in sys.path:
 	sys.path.insert(0, cmd_folder)
 
 import __builtin__
-__builtin__.DEBUG_CONFIG="/home/kshilov/PycharmProjects/frontend_new_templates/config_debug.py"
+__builtin__.DEBUG_CONFIG="/home/iceberg/heymoose/config_debug.py"
 from heymoose import app
 from heymoose.tests.gen_qa import generate_captcha
 
 if __name__ == '__main__':
 	generate_captcha()
-	app.run(port=8989, debug=True)
+	app.run(host="0.0.0.0",port=8989, debug=True)
 

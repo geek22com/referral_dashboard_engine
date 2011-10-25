e424efc92755e0b6e2f4df918f98da0e41fb72b3
iceberg <iceberg@boss.(none)>
2011-10-25 19:09:18 +0400

    Fix text lines in create_app form in webface

--- runserver.py
+++ runserver.py
@@ -1,10 +1,10 @@
 import sys, os
-cmd_folder = os.path.dirname(os.path.abspath("/home/kshilov/PycharmProjects/frontend/frontend-1.0/heymoose"))
+cmd_folder = os.path.dirname(os.path.abspath("/home/iceberg/heymoose/heymoose"))
 if cmd_folder not in sys.path:
 	sys.path.insert(0, cmd_folder)
 
 import __builtin__
-__builtin__.DEBUG_CONFIG="/home/kshilov/PycharmProjects/frontend/frontend-1.0/config_debug.py"
+__builtin__.DEBUG_CONFIG="/home/iceberg/heymoose/config_debug.py"
 from heymoose import app
 from heymoose.tests.gen_qa import generate_captcha
 

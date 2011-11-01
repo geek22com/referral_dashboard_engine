dbf55d3e1af42081028d367c4051030a7151de9f
Eugeny Slezko <mratozar@gmail.com>
2011-11-01 23:47:25 +0300

    Path hardcoding removed

--- runserver.py
+++ runserver.py
@@ -1,14 +1,19 @@
 import sys, os
-cmd_folder = os.path.dirname(os.path.abspath("/home/kshilov/PycharmProjects/frontend/frontend-1.0/heymoose"))
-if cmd_folder not in sys.path:
-	sys.path.insert(0, cmd_folder)
+
+this_path = os.path.realpath(os.path.dirname(__file__))
+project_path = os.path.join(this_path, "heymoose")
+config_path = os.path.join(this_path, "config_debug.py")
+
+if project_path not in sys.path:
+	sys.path.insert(0, project_path)
 
 import __builtin__
-__builtin__.DEBUG_CONFIG="/home/kshilov/PycharmProjects/frontend/frontend-1.0/config_debug.py"
+__builtin__.DEBUG_CONFIG = config_path
+
+
 from heymoose import app
 from heymoose.tests.gen_qa import generate_captcha
 
 if __name__ == '__main__':
 	generate_captcha()
-	app.run(host="0.0.0.0",port=8989, debug=True)
-
+	app.run(host="0.0.0.0", port=8989, debug=True)

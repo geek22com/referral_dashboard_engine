65a6264aeb18864c4b0e50ffd9e3a21d47605530
Eugeny Slezko <mratozar@gmail.com>
2011-11-14 12:51:36 +0300

    Access to admin interface forbidden for non-admins

--- heymoose/admin/__init__.py
+++ heymoose/admin/__init__.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, g, abort
 
 blueprint = Blueprint('admin', __name__, url_prefix='/admin', 
 					static_folder='static', template_folder='templates')
+
+@blueprint.before_request
+def before_request():
+	if g.user is None or not g.user.is_admin():
+		abort(403)
 
 # Import all views in blueprint for registering in app's url map
 import views

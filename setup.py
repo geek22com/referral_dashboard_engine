a4c26b70b44fc12cae3b013bbb63e881bd0f19e2
Eugeny Slezko <mratozar@gmail.com>
2011-11-29 14:11:26 +0400

    Package data discovery in setup

--- setup.py
+++ setup.py
@@ -6,29 +6,30 @@ from setuptools import setup, find_packages
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def find_package_data(root_dir, exts):
+	data = []
+	for root, _dirnames, filenames in os.walk(root_dir):
+		for filename in filenames:
+			if filename.lower().endswith(exts):
+				data.append(os.path.join(root[len(root_dir)+1:], filename))
+	return {'' : data}
 
 setup(
-    name = "frontend",
-    version = "1.0",
-    author = "HeyMoose",
-    author_email = "heymoose@heymoose.com",
-    description = ("Heymoose frontened "
-                                   "Using heymoose backend flask mongodb uwsgi"),
-    license = "Commercial",
-    keywords = "commercial",
-    url = "https://github.com/kshilov/frontend",
-    packages=find_packages(),
-    include_package_data = True,
-    package_data = {'': ['templates/*.html', 
-			'templates/facebook_app/*.html',
-			'templates/gifts/*.html',
-			'templates/admin/*.html'],
-			'heymoose/admin' : ['static/js/*.js'],
-			'heymoose' : ['static/js/*.js', 'static/css/*.css',
-				'static/facebook_app/js/*.js', 'static/facebook_app/css/*.css',
-				'static/vkontakte_app/js/.js', 'static/vkontakte_app/css/*.css']},
-    install_requires = ['flask>=0.7', 'wtforms', 'flask-mongoalchemy', 'uwsgi', 'lxml', 'restkit', 'amqplib', 'python-dateutil==1.5'],
-    long_description=read('README'),
+	name = "frontend",
+	version = "1.0",
+	author = "HeyMoose",
+	author_email = "heymoose@heymoose.com",
+	description = ("Heymoose frontened "
+	                               "Using heymoose backend flask mongodb uwsgi"),
+	license = "Commercial",
+	keywords = "commercial",
+	url = "https://github.com/kshilov/frontend",
+	packages=find_packages(),
+	include_package_data = True,
+	package_data = find_package_data('./heymoose', ('.html', '.css', '.js', '.png', '.gif', '.jpg')),
+	install_requires = ['flask>=0.7', 'wtforms', 'flask-mongoalchemy', 'uwsgi', 'lxml', 'restkit', 'amqplib', 'python-dateutil==1.5'],
+	long_description=read('README'),
 )
 

33a8fd63ee032112d47d06186308ee81691d0a83
Eugeny Slezko <mratozar@gmail.com>
2011-12-06 18:19:09 +0400

    Removed unused static files and package (#11)

--- heymoose/thirdparty/facebook/data.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Facebook use json, so we don't need python class representation
-
-#User object:
-#   http://developers.facebook.com/docs/reference/api/user/
-
-#Create token class here if it simplify usage

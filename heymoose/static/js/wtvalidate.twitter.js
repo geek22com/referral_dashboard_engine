792847b4e96883d63a0ff915487b319f941277cc
Eugeny Slezko <mratozar@gmail.com>
2011-12-06 20:04:14 +0400

    Common layout for all blueprints (#11)

--- heymoose/static/js/wtvalidate.twitter.js
+++ heymoose/static/js/wtvalidate.twitter.js
@@ -10,3 +10,5 @@ function clearError(field) {
 	field.parent().parent().removeClass('error');
 	field.parent().find('span.help-block').remove();
 }
+
+var twitterValidateHandlers = { error: showError, clear: clearError };

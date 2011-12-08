02336a9317b9c1a8848021bd68d46896b436f415
Eugeny Slezko <mratozar@gmail.com>
2011-12-08 12:20:46 +0400

    Improved orders activation js in Admin (#11)

--- heymoose/admin/static/js/admin.js
+++ heymoose/admin/static/js/admin.js
@@ -1,7 +1,7 @@
 // Javascript version of 'to_icon' macro
 function toIcon(elem, value) {
 	if (value)
-		elem.html('<img src="/admin/static/img/icon-yes.gif" alt="да" />');
+		elem.html('<img src="/static/img/icon-yes.gif" alt="да" />');
 	else
-		elem.html('<img src="/admin/static/img/icon-no.gif" alt="нет" />');
+		elem.html('<img src="/static/img/icon-no.gif" alt="нет" />');
 }

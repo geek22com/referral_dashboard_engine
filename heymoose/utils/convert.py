2b0677b39358c3955e6197c15773825ff9563639
Eugeny Slezko <mratozar@gmail.com>
2011-12-13 00:52:47 +0400

    Fixed timezone parsing error when backend returns DateTime in UTC

--- heymoose/utils/convert.py
+++ heymoose/utils/convert.py
@@ -8,7 +8,8 @@ datetime_nosec_format = app.config.get('DATETIME_NOSEC_FORMAT', '%d.%m.%Y %H:%M'
 
 def datetime_from_api(dt):
 	if dt is None: return None
-	return datetime.strptime(dt[:-6], '%Y-%m-%dT%H:%M:%S.%f')
+	without_tz = dt[:-1] if dt.endswith('Z') else dt[:-6]
+	return datetime.strptime(without_tz, '%Y-%m-%dT%H:%M:%S.%f')
 
 def to_datetime(value):
 	return datetime.strptime(str(value), datetime_format)

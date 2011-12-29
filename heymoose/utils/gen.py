bcccf5da15dab8291b17819628e2447bc994275f
Eugeny Slezko <mratozar@gmail.com>
2011-12-29 23:24:19 +0400

    base64 -> base16 in ref links parameter

--- heymoose/utils/gen.py
+++ heymoose/utils/gen.py
@@ -9,9 +9,11 @@ def generate_uid(size, chars=string.ascii_letters+string.digits):
 def aes_base64_encrypt(key, data):
 	obj = AES.new(key, AES.MODE_ECB)
 	bytes = obj.encrypt(str(data))
-	return base64.urlsafe_b64encode(bytes)
+	#return base64.urlsafe_b64encode(bytes)
+	return base64.b16encode(bytes).lower()
 
 def aes_base64_decrypt(key, data):
 	obj = AES.new(key, AES.MODE_ECB)
-	bytes = base64.urlsafe_b64decode(str(data))
+	#bytes = base64.urlsafe_b64decode(str(data))
+	bytes = base64.b16decode(str(data), True)
 	return obj.decrypt(bytes)

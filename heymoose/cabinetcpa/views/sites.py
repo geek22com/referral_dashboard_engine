ffdf6e4c9694ceae3678237f844b67226b83b1ea
Eugeny Slezko <mratozar@gmail.com>
2012-03-28 17:17:55 +0400

    Sites info

--- heymoose/cabinetcpa/views/sites.py
+++ heymoose/cabinetcpa/views/sites.py
@@ -18,4 +18,18 @@ def sites_new():
 	if request.method == 'POST' and form.validate():
 		flash(u'Все ОК', 'success')
 	return render_template('cabinetcpa/sites/new.html', form=form)
-		
+
+@bp.route('/sites/<int:id>')
+@affiliate_only
+def sites_info(id):
+	return render_template('cabinetcpa/sites/info/info.html', site=site)
+
+@bp.route('/sites/<int:id>/edit')
+@affiliate_only
+def sites_info_edit(id):
+	return render_template('cabinetcpa/sites/info/edit.html', site=site)
+
+@bp.route('/sites/<int:id>/stats')
+@affiliate_only
+def sites_info_stats(id):
+	return render_template('cabinetcpa/sites/info/stats.html', site=site)

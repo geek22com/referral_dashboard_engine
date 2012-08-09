d28b9737cc1e7e4eb9445648f6349799ee629b4a
Eugeny Slezko <mratozar@gmail.com>
2012-08-09 19:41:18 +0400

    Colored states in XLS

--- heymoose/views/xls.py
+++ heymoose/views/xls.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 import xlwt, cStringIO
+from heymoose.data.enums import OfferActionStates
 
 bold_font = xlwt.Font(); bold_font.bold = True
+red_font = xlwt.Font(); red_font.colour_index = 2
+green_font = xlwt.Font(); green_font.colour_index = 3
+
 bold_style = xlwt.XFStyle(); bold_style.font = bold_font
+red_style = xlwt.XFStyle(); red_style.font = red_font
+green_style = xlwt.XFStyle(); green_style.font = green_font
 datetime_style = xlwt.XFStyle(); datetime_style.num_format_str = u'dd.mm.yyyy hh:mm:ss'
 currency_style = xlwt.XFStyle(); currency_style.num_format_str = u'# ##0.00 [$руб.-419]'
 
+
 def offer_actions_to_xls(actions):
+	state_styles = { OfferActionStates.APPROVED : green_style, OfferActionStates.CANCELED : red_style }
 	wb = xlwt.Workbook()
 	ws = wb.add_sheet(u'Действия')
 	ws.write(0, 0, u'Время', bold_style)
@@ -20,7 +28,7 @@ def offer_actions_to_xls(actions):
 		ws.write(row, 1, action.transaction_id)
 		ws.write(row, 2, action.offer.code)
 		ws.write(row, 3, action.amount, currency_style)
-		ws.write(row, 4, action.state.name)
+		ws.write(row, 4, action.state.name, state_styles.get(action.state, xlwt.Style.default_style))
 	for i in range(5):
 		ws.col(i).width = 5000
 	f = cStringIO.StringIO()

d811fe598c82fee33d755b4b728c4cf50b534a51
Eugeny Slezko <mratozar@gmail.com>
2012-09-10 18:35:33 +0400

    Affiliate index page

--- heymoose/cabinetcpa/views/base.py
+++ heymoose/cabinetcpa/views/base.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
-from flask import g, redirect, url_for, abort
-from heymoose import app
+from flask import g, abort
 from heymoose.cabinetcpa import blueprint as bp
+from heymoose.views.decorators import template
 
+
+@template('cabinetcpa/index-affiliate.html')
+def index_affiliate():
+	return dict()
+
+@template('cabinetcpa/index-advertiser.html')
+def index_advertiser():
+	return dict()
 
 @bp.route('/')
 def index():
-	if g.user.is_affiliate:
-		return redirect(url_for('.offers_requested'))
-	elif g.user.is_advertiser:
-		return redirect(url_for('.offers_list'))
+	if g.user.is_advertiser:
+		return index_advertiser()
+	elif g.user.is_affiliate:
+		return index_affiliate()
 	else:
-		app.logger.error("Shit happened: user has unknown role in user cabinet")
 		abort(403)
+	
+	
 
 
 

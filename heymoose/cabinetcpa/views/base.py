20a3e20c0ce24948a59e185eedd536d9f93585aa
Eugeny Slezko <mratozar@gmail.com>
2012-09-10 20:21:36 +0400

    Affiliate index page

--- heymoose/cabinetcpa/views/base.py
+++ heymoose/cabinetcpa/views/base.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 from flask import g, abort
+from heymoose import resource as rc
 from heymoose.cabinetcpa import blueprint as bp
+from heymoose.forms import forms
 from heymoose.views.decorators import template
+from heymoose.db.models import Notification
 
 
 @template('cabinetcpa/index-affiliate.html')
 def index_affiliate():
-	return dict()
+	form = forms.DateTimeRangeForm()
+	args = dict(offset=0, limit=5, order='CONFIRMED_REVENUE', direction='DESC', **form.backend_args())
+	stats, _ = rc.offer_stats.list_user(g.user, **args)
+	
+	user_notifications_query = Notification.query.filter(Notification.user_id == g.user.id)
+	notifications = user_notifications_query.descending(Notification.date).limit(5).all()
+	
+	return dict(stats=stats, notifications=notifications)
 
 @template('cabinetcpa/index-advertiser.html')
 def index_advertiser():

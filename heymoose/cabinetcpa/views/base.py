4e2ba11f67832d2f95d082bb89fb18c49d64983d
Eugeny Slezko <mratozar@gmail.com>
2012-09-11 15:00:06 +0400

    Affiliate cabinet index page

--- heymoose/cabinetcpa/views/base.py
+++ heymoose/cabinetcpa/views/base.py
@@ -4,7 +4,7 @@ from heymoose import resource as rc
 from heymoose.cabinetcpa import blueprint as bp
 from heymoose.forms import forms
 from heymoose.views.decorators import template
-from heymoose.db.models import Notification
+from heymoose.db.models import Notification, NewsItem
 
 
 @template('cabinetcpa/index-affiliate.html')
@@ -12,15 +12,14 @@ def index_affiliate():
 	form = forms.DateTimeRangeForm()
 	args = dict(offset=0, limit=5, order='CONFIRMED_REVENUE', direction='DESC', **form.backend_args())
 	stats, _ = rc.offer_stats.list_user(g.user, **args)
-	
 	user_notifications_query = Notification.query.filter(Notification.user_id == g.user.id)
 	notifications = user_notifications_query.descending(Notification.date).limit(5).all()
-	
-	return dict(stats=stats, notifications=notifications)
+	news = NewsItem.query.filter(NewsItem.active == True).descending(NewsItem.date).limit(3).all()
+	return dict(stats=stats, notifications=notifications, news=news)
 
 @template('cabinetcpa/index-advertiser.html')
 def index_advertiser():
-	return dict()
+	return 'OK'
 
 @bp.route('/')
 def index():

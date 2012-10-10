c1edbb037e78b1238ccbd41c5668f7d917a8926e
Eugeny Slezko <mratozar@gmail.com>
2012-10-10 13:40:44 +0400

    Fixed JS compression

--- heymoose/static/js/bootstrap-dropdown.js
+++ heymoose/static/js/bootstrap-dropdown.js
@@ -27,11 +27,11 @@
     return this.each(function () {
       $(this).delegate(selector || d, 'click', function (e) {
         var li = $(this).parent('li')
-          , isActive = li.hasClass('open')
+          , isActive = li.hasClass('open');
 
-        clearMenus()
-        !isActive && li.toggleClass('open')
-        return false
+        clearMenus();
+        !isActive && li.toggleClass('open');
+        return false;
       })
     })
   }
@@ -42,12 +42,12 @@
   var d = 'a.menu, .dropdown-toggle'
 
   function clearMenus() {
-    $(d).parent('li').removeClass('open')
+    $(d).parent('li').removeClass('open');
   }
 
   $(function () {
-    $('html').bind("click", clearMenus)
-    $('body').dropdown( '[data-dropdown] a.menu, [data-dropdown] .dropdown-toggle' )
+    $('html').bind("click", clearMenus);
+    $('body').dropdown( '[data-dropdown] a.menu, [data-dropdown] .dropdown-toggle' );
   })
 
 }( window.jQuery || window.ender );

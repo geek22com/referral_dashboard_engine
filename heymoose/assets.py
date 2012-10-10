025be997ea160db5b1b44fbe9fdedd5d439b1a01
Eugeny Slezko <mratozar@gmail.com>
2012-10-10 12:59:53 +0400

    Assets for Cabinet and Admin

--- heymoose/assets.py
+++ heymoose/assets.py
@@ -13,7 +13,7 @@ def configure_assets(app):
 		'ak/js/jquery.tmpl.min.js',
 		'js/wtvalidate2.js',
 		'ak/js/common.js',
-		filters=['rjsmin'], output='ak/js/site.js'
+		filters=['rjsmin'], output='ak/js/site.gen.js'
 	))
 	assets.register('site_css', Bundle(
 		'ak/css/default.css',
@@ -23,9 +23,30 @@ def configure_assets(app):
 		'ak/css/jquery.fancybox.css',
 		'ak/css/ui.notify.css',
 		'ak/css/override.css',
-		output='ak/css/site.css'
+		output='ak/css/site.gen.css'
 	))
 	assets.register('site_ie_css', Bundle(
 		'ak/css/ie.css',
-		output='ak/css/site.ie.css'
+		output='ak/css/site.ie.gen.css'
+	))
+	assets.register('cabinet_js', Bundle(
+		'js/jquery-latest.js',
+		'js/jquery.form.js',
+		'js/bootstrap-dropdown.js',
+		'js/bootstrap-modal.js',
+		'js/bootstrap-alerts.js',
+		'js/wtvalidate2.js',
+		'js/wtvalidate2.twitter.js',
+		filters=['rjsmin'], output='js/cabinet.gen.js'
+	))
+	assets.register('cabinet_css', Bundle(
+		'css/bootstrap.min.css',
+		'css/bootstrap.hm.css',
+		'css/roundbox.css',
+		'css/lists.css',
+		'css/forms.css',
+		'css/pills-filter.css',
+		'css/redactor-text.css',
+		'css/disqus.css',
+		output='css/cabinet.gen.css'
 	))

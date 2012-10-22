af6fe3d3dae4f0ab6a7f20e31ed399dbae3dd240
Eugeny Slezko <mratozar@gmail.com>
2012-10-22 16:54:41 +0400

    Permissions configuration

--- config_development.py
+++ config_development.py
@@ -89,18 +89,52 @@ ROBOKASSA_WMID = '276669831570'
 # Admin permissions parameters
 SUPER_ADMINS = ['admin@heymoose.com']
 ADMIN_GROUPS = {
-	u'Аккаунт-менеджеры': set(['view_finances', 'do_withdrawals']),
-	u'Поддержка рекламодателей': set(['login_to_advertiser']),
+	u'Сверки': set([
+		'view_advertiser',
+		'view_advertiser_offers',
+		'view_advertiser_finances',
+		'view_advertiser_stats',
+		'view_offer_sales',
+		'view_offer_stats'
+	]),
+	u'Поддержка рекламодателей': set([
+		'view_advertiser',
+		'view_advertiser_offers',
+		'view_advertiser_finances',
+		'view_advertiser_stats',
+		'do_advertiser_edit',
+		'do_advertiser_login',
+		'do_advertiser_block',
+		'view_offer_sales',
+		'view_offer_requests',
+		'view_offer_stats',
+		'do_offer_edit',
+	]),
 	u'Поддержка партнёров': set([
 		'view_fraud',
 		'view_affiliate',
 		'view_affiliate_offers',
 		'view_affiliate_finances',
 		'view_affiliate_referrals',
+		'view_affiliate_stats',
 		'do_affiliate_edit',
 		'do_affiliate_login',
-		'do_affiliate_block'
+		'do_affiliate_block',
+		'view_offer_requests'
 	]),
-	u'Анализ трафика': set(['view_fraud']),
-	u'PR': set([])
+	u'Анализ трафика': set([
+		'view_fraud',
+		'view_affiliate',
+		'view_affiliate_offers',
+		'view_affiliate_stats',
+		'do_affiliate_block',
+		'view_offer_requests',
+		'view_offer_stats'
+	]),
+	u'PR': set([
+		'view_affiliate',
+		'view_affiliate_offers',
+		'view_advertiser',
+		'view_advertiser_offers'
+	])
 }

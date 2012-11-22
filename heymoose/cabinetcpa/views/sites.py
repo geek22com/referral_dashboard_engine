bb5c2d7679bc3e4667ce70c69f7d64bd188b86d1
Eugeny Slezko <mratozar@gmail.com>
2012-11-22 12:23:00 +0400

    Site creation

--- heymoose/cabinetcpa/views/sites.py
+++ heymoose/cabinetcpa/views/sites.py
@@ -1,35 +1,49 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, request, flash
+from flask import g, request, redirect, url_for, flash
+from heymoose import app, resource as rc
+from heymoose.data.models import Site
 from heymoose.forms import forms
 from heymoose.cabinetcpa import blueprint as bp
 from heymoose.cabinetcpa.decorators import affiliate_only
+from heymoose.views.decorators import template, sorted, paginated
 
-site = dict(id=1)
+
+SITES_PER_PAGE = app.config.get('SITES_PER_PAGE', 20)
+
 
 @bp.route('/sites/')
 @affiliate_only
-def sites_list():
-	return render_template('cabinetcpa/sites/list.html')
+@template('cabinetcpa/sites/list.html')
+@sorted('creation_time', 'desc')
+@paginated(SITES_PER_PAGE)
+def sites_list(**kwargs):
+	sites, count = rc.sites.list(**kwargs)
+	return dict(sites=sites, count=count)
 
-@bp.route('/sites/new')
+
+@bp.route('/sites/new/', methods=['GET', 'POST'])
 @affiliate_only
+@template('cabinetcpa/sites/new.html')
 def sites_new():
-	form = forms.SiteForm()
+	site_type = request.args.get('type', '').upper()
+	if not site_type: return dict()
+	form = forms.site_form_by_type(site_type, request.form)
 	if request.method == 'POST' and form.validate():
-		flash(u'Все ОК', 'success')
-	return render_template('cabinetcpa/sites/new.html', form=form)
+		site = Site(affiliate=g.user, type=site_type)
+		form.populate_obj(site)
+		rc.sites.add(site)
+		flash(u'Площадка успешно добавлена', 'success')
+		return redirect(url_for('.sites_list'))
+	return dict(form=form)
 
-@bp.route('/sites/<int:id>')
+
+@bp.route('/sites/<int:id>/')
 @affiliate_only
 def sites_info(id):
 	return render_template('cabinetcpa/sites/info/info.html', site=site)
 
-@bp.route('/sites/<int:id>/edit')
+
+@bp.route('/sites/<int:id>/edit/')
 @affiliate_only
 def sites_info_edit(id):
 	return render_template('cabinetcpa/sites/info/edit.html', site=site)
-
-@bp.route('/sites/<int:id>/stats')
-@affiliate_only
-def sites_info_stats(id):
-	return render_template('cabinetcpa/sites/info/stats.html', site=site)

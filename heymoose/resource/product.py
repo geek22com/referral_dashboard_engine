b0ce3baf3ac4ab415ed7e10f4b876c6eb5855f55
Eugeny Slezko <mratozar@gmail.com>
2012-11-27 16:34:58 +0400

    Filter products by site

--- heymoose/resource/product.py
+++ heymoose/resource/product.py
@@ -11,5 +11,5 @@ class ProductResource(BackendResource):
 	def feed_size(self, key, **kwargs):
 		return self.path('feed').path('size').get(key=key, **kwargs).as_int()
 
-	def categories(self, aff_id):
-		return self.path('categories').get(aff_id=aff_id).as_objlist(Shop)
+	def categories(self, aff_id, **kwargs):
+		return self.path('categories').get(aff_id=aff_id, **kwargs).as_objlist(Shop)

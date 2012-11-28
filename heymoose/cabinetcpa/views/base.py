ada29571bc3c77f6b291b64f4a61c826e5aaf2a6
Eugeny Slezko <mratozar@gmail.com>
2012-11-28 19:47:18 +0400

    Site stats in Cabinet dashboard

--- heymoose/cabinetcpa/views/base.py
+++ heymoose/cabinetcpa/views/base.py
@@ -11,15 +11,16 @@ from heymoose.data.mongo.models import Notification, NewsItem
 def index_affiliate():
 	form = forms.DateTimeRangeForm()
 	args = dict(offset=0, limit=5, ordering='NOT_CONFIRMED_REVENUE', direction='DESC', **form.backend_args())
-	stats, _ = rc.offer_stats.list_user(g.user, **args)
+	offer_stats, _ = rc.offer_stats.list_user(g.user, **args)
+	site_stats, _ = rc.offer_stats.list_by_site(aff_id=g.user.id, **args)
 	user_notifications_query = Notification.query.filter(Notification.user_id == g.user.id)
 	notifications = user_notifications_query.descending(Notification.date).limit(5).all()
 	news = NewsItem.query.filter(NewsItem.active == True).descending(NewsItem.date).limit(3).all()
 	has_sites, _ = rc.sites.list(aff_id=g.user.id, offset=0, limit=1)
 	has_placements, _ = rc.placements.list(aff_id=g.user.id, offset=0, limit=1)
 	referral_offer = rc.offers.get_referral_offer() if request.args.get('registered') == g.user.referral_code else None
-	return dict(stats=stats, notifications=notifications, news=news, referral_offer=referral_offer,
-		has_sites=has_sites, has_placements=has_placements)
+	return dict(offer_stats=offer_stats, site_stats=site_stats, notifications=notifications, news=news,
+		referral_offer=referral_offer, has_sites=has_sites, has_placements=has_placements)
 
 @template('cabinetcpa/index-advertiser.html')
 def index_advertiser():
